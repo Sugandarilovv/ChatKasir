9c9d862c3a5533ddcda452c6b5eacad0eca17046
reihanersaa <cptprice7778@gmail.com>
2026-04-28 01:13:34 +0700

    bikin endpoint POST untuk /auth/register. setup rute ke database.

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./src/routes/authRoutes");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "ChatKasir API is running!" });
