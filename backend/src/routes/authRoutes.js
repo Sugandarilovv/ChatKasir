18c26a449e97ac96e04cf9db8eff34a039f687e8
reihanersaa <cptprice7778@gmail.com>
2026-05-12 13:15:29 +0700

    feat(fs2): adding profile and picture update endpoints.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const { body } = require("express-validator");
+const { register, login, verifyOtp } = require("../controllers/authController");
 
-router.post("/register", register);
+// Validasi untuk Register
+const registerValidation = [
+  body("email").isEmail().withMessage("Format email tidak valid"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password minimal 6 karakter"),
+  body("full_name").notEmpty().withMessage("Nama lengkap wajib diisi"),
+];
+
+router.post("/register", registerValidation, register);
 router.post("/login", login);
+router.post("/verify-otp", verifyOtp);
 
 module.exports = router;
