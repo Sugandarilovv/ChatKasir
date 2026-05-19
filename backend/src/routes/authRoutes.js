ad3a243f476c7376ef53c691718bebf7ab692f5e
reihanersaa <cptprice7778@gmail.com>
2026-05-19 12:35:00 +0700

    fix: cleanup updatePassword endpoint

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const {
   login,
   verifyOtp,
   forgotPassword,
+  updatePassword,
 } = require("../controllers/authController");
 
 // Validasi untuk Register
@@ -21,5 +22,7 @@ router.post("/register", registerValidation, register);
 router.post("/login", login);
 router.post("/verify-otp", verifyOtp);
 router.post("/forgot-password", forgotPassword);
+
+router.put("/update-password", updatePassword);
 
 module.exports = router;
