304310cf85e77a60388fdce8e2ee4bf4eb51bffb
reihanersaa <cptprice7778@gmail.com>
2026-05-21 11:07:49 +0700

    fix: update bulletproof cors with vercel preview support

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const transactionRoutes = require("./src/routes/transactionRoutes");
 const reportRoutes = require("./src/routes/reportRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 
+const app = express();
+
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -16,9 +18,21 @@ app.use(
         process.env.FRONTEND_URL,
       ];
 
-      if (!origin || allowedOrigins.includes(origin)) {
+      const cleanOrigins = allowedOrigins.map((url) =>
+        url ? url.replace(/\/$/, "") : url,
+      );
+      const cleanOrigin = origin ? origin.replace(/\/$/, "") : origin;
+
+      // buat bersihin tanda slash
+      if (
+        !origin ||
+        cleanOrigins.includes(cleanOrigin) ||
+        cleanOrigin.endsWith(".vercel.app")
+      ) {
+        // Baris ".endsWith" di atas otomatis mengizinkan semua domain vercel milik Alfan termasuk link preview-nya!
         callback(null, true);
       } else {
+        console.log("Origin yang diblokir oleh CORS:", origin);
         callback(new Error("Not allowed by CORS"));
       }
     },
